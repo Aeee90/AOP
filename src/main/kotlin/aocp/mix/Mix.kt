378c5bb52ffe2aa356bea59f7322cdb43413fafe
babo72 <babo72@gmail.com>
2019-04-18 17:48:20 +0900

    making...

---
+++ src/main/kotlin/aocp/mix/Mix.kt
@@ -0,0 +1,16 @@
+package aocp.mix
+
+object Mix {
+
+    private val A = Register(5)
+    private val X = Register(5)
+
+    private val I1 = Register(2)
+    private val I2 = Register(2)
+    private val I3 = Register(2)
+    private val I4 = Register(2)
+    private val I5 = Register(2)
+    private val I6 = Register(2)
+    private val J = Register(2)
+
+}
--- src/main/kotlin/aocp/mix/test.kt
+++
@@ -1,2 +0,0 @@
-package aocp.mix
-
